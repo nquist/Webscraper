090ddb7d8c119a915b05553f764e07428ebe415a
nquist <47543479+nquist@users.noreply.github.com>
2022-03-03 00:13:14 -0800

    updates

--- functions.py
+++ functions.py
@@ -6,6 +6,10 @@ Created on Mon Feb 28 18:20:34 2022
 
 Webscrapper functions
 """
+import requests
+from bs4 import BeautifulSoup
+
+nums = set([str(i) for i in range(30)])
 
 def make_urls(starting_url, ending_url, page_num, btype=1):
     url_list = []
@@ -21,3 +25,120 @@ def make_urls(starting_url, ending_url, page_num, btype=1):
             url_list.append(temp_url)
     
     return url_list
+
+def read_webpage(url, section):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+    
+    soup = BeautifulSoup(response.text, "html.parser")
+    val = soup.find(id=section)
+    holder = str(val).replace('<br>', '<br/>')
+    content = holder.split('<br/>')
+    for i in range(len(content)-1, -1, -1):
+        if content[i].strip() == '':
+            content.pop(i)
+    
+    return content
+
+def clean_content(content):
+    char_dic = {'“':'"', '”':'"', '’':"'", '—':'--', '…':'...', '‘':"'", '�':'', 'ï':'&#239;', 'é':'&#233;', '–':'-', 'ç':'&#231;', 'ü':'&#252;'}
+    chars = ['“', '”', '’', '—', '…', '‘', '�', 'ï', 'é', '–', 'ç', 'ü']
+    data_content = 1*content
+    types = {'p', 'b', 'strong', 'i', 'em'}
+    clean_content = []
+    for i in range(len(data_content)):
+        line = data_content[i].strip()
+        if '>' in line:
+            new_line = ''
+            temp = line.split('>')
+            for part in temp:
+                part_clean = part.strip()
+                if part_clean != '':
+                    start = 0
+                    if part_clean[:2] == '</':
+                        start = 2
+                    elif part_clean[0] == '<':
+                        start = 1
+                    elif 'google' not in part_clean:
+                        new_line += part.strip()
+                    if ' ' in part_clean:
+                        end = part_clean.index(' ')
+                    else:
+                        end = len(part_clean)
+        
+                    if start !=0:
+                        tpe = part_clean[start:end]
+                        if tpe in types:
+                            new_line += part + '>'
+            if new_line != '':
+                for char in chars:
+                    new_line = new_line.replace(char, char_dic[char])
+                if new_line != '':
+                    clean_content.append(new_line)
+        else:
+            for char in chars:
+                line = line.replace(char, char_dic[char])
+            if line != '':
+                clean_content.append(line)
+        
+    
+    return clean_content[:-1]
+
+def format_text(content, skip=0, num = False):
+    chapters = []
+    starts = [0]
+    if num:
+        for i in range(skip, len(content)-1):
+            if content[i].strip() in nums:
+                content[i] = 'Chapter ' + content[i]
+                starts.append(i)
+    else:
+        for i in range(skip, len(content)-1):
+            if content[i][:7].lower() == 'chapter':
+                if content[i][9] not in nums:
+                    starts.append(i)
+            elif content[i][:7].lower() == 'epilogu':
+                starts.append(i)
+            elif content[i][:7].lower() == 'prologu':
+                starts.append(i)
+    
+    starts.append(len(content))
+    
+    skip = False
+    for i in range(len(starts)-1):
+        line = ''
+        for j in range(starts[i+1]-starts[i]):
+            if not skip:
+                if j == 0 and (content[starts[i]+j].lower()[:7] == 'chapter' or content[starts[i]+j].lower()[:7] == 'epilogu' or content[starts[i]+j].lower()[:7] == 'prologu'):
+                    line += '<h2>' + content[starts[i]+j] + '</h2>\n'
+                    line += '<p><b>' + content[starts[i]+j+1] + '</b></p>\n\n'
+                    skip = True
+                else:
+                    if starts[i]+j+1 < len(content):
+                        nx_line = content[starts[i]+j+1]
+                    else:
+                        nx_line = 'A'
+                    if nx_line[0] != '"' and (nx_line[0].lower() == nx_line[0]):
+                        line += '<p>' + content[starts[i]+j] + ' ' + content[starts[i]+j+1] + '</p>\n\n'
+                        skip = True
+                    else:
+                        line += '<p>' + content[starts[i]+j] + '</p>\n\n'
+            else:
+                skip = False
+        chapters.append(line)
+    return chapters
+
+def write_xhtml(chapter, title, number):
+    f = open('Chapter'+number+'.xhtml', 'w')
+    
+    f.write('<html xmlns="http://www.w3.org/1999/xhtml">\n\n')
+    f.write('<head>\n\n')
+    f.write('<title>' + title +'</title>\n\n')
+    f.write('</head>\n')
+    f.write('<body>\n')
+    f.write(chapter+'\n')
+    f.write('</body>\n')
+    f.write('</html>\n')
+    
+    f.close()
+    return True
--- webscraper.py
+++ webscraper.py
@@ -8,10 +8,63 @@ Loads website information from ProjectInfo.txt, scrapes the info from the websit
 and then formats it into text files and saves it at the file location.
 """
 import functions
+import os
 
-import requests
-import urllib.request
+creation = True
+numbering = True
 
 f = open('ProjectInfo.txt', 'r')
 lines = f.readlines()
-urls = functions.make_urls(lines[5][:-1], lines[7][:-1], lines[9][:-1])
+f.close()
+
+folder_name = lines[1].replace(" ", '').strip()
+folder_path = lines[11].strip() + '\\' + folder_name
+if not os.path.isdir(folder_path):
+    os.mkdir(folder_path)
+if not os.path.isfile(lines[11].strip() + '\\' + 'ProjectInfo.txt'):
+    src ='ProjectInfo.txt'
+    dst = lines[11].strip() + '\\' + folder_name + '\\' + 'ProjectInfo.txt'
+    os.popen(f"copy {src} {dst}")
+os.chdir(folder_path)
+
+if creation:
+    urls = functions.make_urls(lines[5][:-1], lines[7][:-1], lines[9][:-1])
+    offset = int(lines[13])
+
+    remnent = ''
+    ch_count = 1
+    for i in range(len(urls)):
+        content = functions.read_webpage(urls[i], "textToRead")
+        content = functions.clean_content(content)
+        if i == 0:
+            content = functions.format_text(content, skip = offset, num=numbering)
+            file = open('pretext.txt', 'w')
+            file.write(content[0])
+            file.close()
+            content = content[1:]
+        else:
+            content = functions.format_text(content, num=numbering)
+                
+        content[0] = remnent + content[0]
+        for j in range(len(content)-1):
+            chapter = content[j]
+            if len(str(ch_count))==1:
+                chapter_num = '00' + str(ch_count)
+            elif len(str(ch_count))==2:
+                chapter_num = '0' + str(ch_count)
+            else:
+                chapter_num = str(ch_count)
+        
+            idx = chapter.index('/')
+            chapter_title = chapter[4:idx-1]
+        
+            ret = functions.write_xhtml(chapter, chapter_title, chapter_num)
+            ch_count += 1
+    
+        remnent = content[-1]
+
+    chapter_num = '0' + str(ch_count)
+    idx = chapter.index('/')
+    chapter_title = chapter[4:idx-1]
+    ret = functions.write_xhtml(remnent, chapter_title, chapter_num)
+
